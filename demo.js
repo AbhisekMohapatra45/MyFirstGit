6987f1212e356e300ae43db1fb008de407ba9d54
AbhisekMohapatra45 <abhisekmohapatra45@gmail.com>
2022-04-29 11:58:22 +0530

    Write Another Arrow Function

--- demo.js
+++ demo.js
@@ -1,5 +1,17 @@
-var arr = [2,4,6,8].every((elements)=>{
-    return elements%2==0;
-});
+// var arr = [2,4,6,8].every((elements)=>{
+//     return elements%2==0;
+// });
 
-console.log(arr)
+// console.log(arr)
+
+var newFunction = ((Number) => {
+    if (Number % 2 === 0) {
+        console.log('IsEven')
+        return
+    }
+    else {
+        console.log('IsOdd')
+    }
+})
+
+newFunction(20)
